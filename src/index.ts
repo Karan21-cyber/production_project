5eece86a18b533f4923551253b0c7cc9934ad028
Karan Chaudhary <karanchaudhary@Karans-MacBook-Pro.local>
2024-03-04 22:13:59 +0545

    chagnes

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,26 @@
-import express, { type Request, type Response } from 'express'
-import prisma from './prisma'
-import errorMiddleware from './middleware/error-middleware'
-import authRouter from './router/auth-router'
-import createSubscriptionProduct from './service/payment-sevices'
-import userRouter from './router/user-router'
-import workspaceRouter from './router/workspace-router'
-import foldersRouter from './router/folder-router'
-import filesRouter from './router/files-router'
+import express, { type Request, type Response } from "express";
+import prisma from "./prisma";
+import errorMiddleware from "./middleware/error-middleware";
+import authRouter from "./router/auth-router";
+import createSubscriptionProduct from "./service/payment-sevices";
+import userRouter from "./router/user-router";
+import workspaceRouter from "./router/workspace-router";
+import foldersRouter from "./router/folder-router";
+import filesRouter from "./router/files-router";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
- 
+app.get("/", (req: Request, res: Response) => {
   res.json({
     success: true,
     Status_Code: 200,
-    message: 'Welcome to Production Project Server...'
-  })
-})
+    message: "Welcome to Production Project Server...",
+  });
+});
 
-app.use(authRouter,userRouter,workspaceRouter,foldersRouter,filesRouter)
-app.use(errorMiddleware)
+app.use(authRouter, userRouter, workspaceRouter, foldersRouter, filesRouter);
+app.use(errorMiddleware);
 
 createSubscriptionProduct();
 
@@ -29,11 +28,9 @@ prisma
   .$connect()
   .then(() => {
     app.listen(5500, () => {
-      console.log('Server started on port 5500')
-    })
+      console.log("Server started on port 5500");
+    });
   })
   .catch(async (e: Error) => {
-    console.log(e.message)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.log(e.message);
+  });
